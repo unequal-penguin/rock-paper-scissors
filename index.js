42292d8aaf5b914b08b12bbf9c319d92b4c62c10
unequal-penguin <heitor360@live.com>
2023-02-26 14:11:27 -0300

    Add game function to play five times
     - Add helper function capitalize for formatting messages
    
     - Add functions to get and validate player choice from prompt

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const Choices = {
   Scissors: 'scissors'
 }
 
+function game() {
+  for (let index = 0; index < 5; index++) {
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+
+    let roundResult = playRound(playerChoice, computerChoice);
+
+    console.log("**************************************************");
+    console.log(`Player choice: ${playerChoice}, Computer choice: ${computerChoice}`);
+    console.log(roundResult);
+  }
+}
+
 function playRound(playerChoice, computerChoice) {
   playerChoice = playerChoice.toLowerCase();
   computerChoice = computerChoice.toLowerCase();
@@ -16,19 +29,39 @@ function playRound(playerChoice, computerChoice) {
     return "It's a tie!";
 
   else if (playerWinCondition)
-    return `You win! ${playerChoice} beats ${computerChoice}`;
+    return `You win! ${capitalize(playerChoice)} beats ${computerChoice}`;
 
   else
-    return `You lose! ${computerChoice} beats ${playerChoice}`;
+    return `You lose! ${capitalize(computerChoice)} beats ${playerChoice}`;
 }
 
 function getComputerChoice() {
-  const choices = [Choices.Rock, Choices.Paper, Choices.Scissors];
-  const randomChoiceIndex = Math.floor(Math.random() * choices.length);
-  return choices[randomChoiceIndex];
+  const choicesList = [Choices.Rock, Choices.Paper, Choices.Scissors];
+  const randomChoiceIndex = Math.floor(Math.random() * choicesList.length);
+
+  return choicesList[randomChoiceIndex];
 }
 
-const playerChoice = 'ROCK';
-const computerChoice = getComputerChoice();
+function getPlayerChoice() {
+  let validPlayerChoice = false;
+  let playerChoice = '';
 
-console.log(playRound(playerChoice, computerChoice));
+  while (!validPlayerChoice) {
+    playerChoice = prompt('Choose rock, paper, or scissors:');
+    validPlayerChoice = validatePlayerChoice(playerChoice);
+  }
+
+  return playerChoice;
+}
+
+function validatePlayerChoice(playerChoice) {
+  const choicesList = [Choices.Rock, Choices.Paper, Choices.Scissors];
+
+  return choicesList.includes(playerChoice.toLowerCase());
+}
+
+function capitalize(text) {
+  return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+}
+
+game();
