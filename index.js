961a09577f57356222e2abfa7a4a83e3af98d490
unequal-penguin <heitor360@live.com>
2023-02-26 13:16:36 -0300

    Add enum Choices and getComputerChoice method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+const Choices = {
+  Rock: 'Rock',
+  Paper: 'Paper',
+  Scissors: 'Scissors'
+}
+
+function getComputerChoice() {
+  const choices = [Choices.Rock, Choices.Paper, Choices.Scissors];
+  const randomChoiceIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomChoiceIndex];
+}
+
+console.log(getComputerChoice());
